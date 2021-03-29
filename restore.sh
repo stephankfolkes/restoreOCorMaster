77bab0dea043e7a3b67998ed98b626451c6d095b
Stephan Folkes <sfolkes@cloudbees.com>
2021-03-29 15:15:45 +0100

    Added support for S3 and local backups, Changed scruupt execution options, different rescue pods for each statefulset

--- restore.sh
+++ restore.sh
@@ -1,16 +1,76 @@
 #!/bin/bash
 set -e
 
-namespace=$1 #Kubernetes namespace where CB CI is deployed.
-instanceStatefulsetName=$2 #The statefulset name of the OC or master deployment.
-s3Bucket=$3 #S3 bucketname containing backups.
-backupFilePath=$4 #Path of the tar.qz file within the S3 bucket.
-rescueContainerImage=$5 #Container with version if required, to use for the rescue pod.
+namespace="" #Kubernetes namespace where CB CI is deployed.
+instanceStatefulsetName="" #The statefulset name of the OC or master deployment.
+s3FilePath="" #Path of the tar.gz file within the S3 bucket.
+backupSource="" #source of backups. local or s3
+s3BucketName="" #S3 bucketname containing backups.
+backupFilePath="" #Path to the tar.gz backup file on the local directory
+rescueContainerImage="governmentpaas/awscli" #Container with version if required, to use for the rescue pod.
 
-if [ -z $namespace ] || [ -z $instanceStatefulsetName ] || [ -z $s3Bucket ] || [ -z $backupFilePath ] || [ -z $rescueContainerImage ]
+help(){
+  echo -e "restore.sh Help\nUsage: restore.sh [parameters]\nParameters:\n--namepace <Kubernetes namespace>\n--instanceStatefulsetName <Operations Center or Controller statefulset name>\n--backupSource <local or s3>\n--backupFilePath <location of local backup file, tar.gz>\n--s3BucketName <S3 bucket name>\n--s3FilePath <Path to file in S3 bucket, tar,gz>"
+}
+
+#Add validation for each parameter
+while [ -n "$1" ]; do
+	case "$1" in
+	--namespace)
+    namespace="$2"
+    shift
+    ;;
+	--instanceStatefulsetName)
+		instanceStatefulsetName="$2"
+		shift
+		;;
+	--s3FilePath)
+    s3FilePath="$2"
+    shift
+    ;;
+  --backupSource)
+    backupSource="$2"
+    shift
+    ;;
+  --s3BucketName)
+    s3BucketName="$2"
+    shift
+    ;;
+  --backupFilePath)
+    backupFilePath="$2"
+    shift
+    ;;
+  --rescueContainerImage)
+    rescueContainerImage="$2"
+    shift
+    ;;
+	*) echo "Option $1 not recognized" && help && exit 1 ;;
+	esac
+	shift
+done
+
+if [ -z $namespace ] || [ -z $instanceStatefulsetName ] || [ -z $backupSource ] || [ -z $rescueContainerImage ]
 then
-  echo "Execution parameters missing. Loading variables from config file."
-  source config
+  echo "Execution parameters missing. Ignoring parameter inputs. Loading variables from config file."
+#  source config
+fi
+
+if [ $backupSource = "s3" ]
+then
+  if [ -z $s3FilePath ] || [ -z $s3BucketName ]
+  then
+    echo "--s3FilePath and --s3BucketName parameters must be configured when using S3 backup source."
+    help
+    exit 1
+  fi
+elif [ $backupSource = "local" ]
+then
+  if [ -z $backupFilePath ]
+  then
+    echo "--backupFilePath parameter must be configured when using the local backup source."
+    help
+    exit 1
+  fi
 fi
 
 echo "Scale down stateful set pods to 0 replicas"
@@ -18,14 +78,16 @@ kubectl --namespace=$namespace scale statefulset/$instanceStatefulsetName --repl
 
 #Launch rescue pod attaching the pvc
 persistentVolumeClaim=$(kubectl -n $namespace get statefulset $instanceStatefulsetName -o jsonpath="{.spec.volumeClaimTemplates[0].metadata.name}")-${instanceStatefulsetName}-0
-echo "Launching rescue-pod with pvc $persistentVolumeClaim attached"
+rescueStorageMountPath="/tmp/jenkins-home"
+rescuePodName=rescue-pod-$instanceStatefulsetName
+echo "Launching $rescuePodName with pvc $persistentVolumeClaim attached"
 
 cat <<EOF | kubectl --namespace=$namespace create -f -
 
 kind: Pod
 apiVersion: v1
 metadata:
-  name: rescue-pod
+  name: $rescuePodName
 spec:
   securityContext:
     runAsUser: 1000
@@ -40,33 +102,42 @@ spec:
       image: $rescueContainerImage
       command: ['sh', '-c', 'echo The app is running! && sleep 100000' ]
       volumeMounts:
-        - mountPath: "/tmp/jenkins-home"
+        - mountPath: $rescueStorageMountPath
           name: rescue-storage
 EOF
 
-echo "Downloading the backup file from S3 into local /tmp directory"
-aws s3 cp s3://${s3Bucket}/${backupFilePath} /tmp/backup.tar.gz
+localBackupFilePath="/tmp/backup.tar.gz" #File path where backups will be uploaded
+case "$backupSource" in
+  s3)
+  echo "Downloading the backup file from S3 into local $localBackupFilePath directory"
+  aws s3 cp s3://${s3BucketName}/${backupFilePath} $localBackupFilePath
+  ;;
+  local)
+  [ -f $backupFilePath ] && echo "Backup file $backupFilePath exists locally." || (echo "Backup file $backupFilePath is missing. Please ensure the file can be found on local or mounted directories." && exit 1)
+  localBackupFilePath=$backupFilePath
+  ;;
+esac
 
-echo "Waiting for the rescue-pod to enter Ready state"
-kubectl wait --namespace=$namespace --for=condition=Ready --timeout=600s pod/rescue-pod
+echo "Waiting for the $rescuePodName to enter Ready state"
+kubectl wait --namespace=$namespace --for=condition=Ready --timeout=600s pod/$rescuePodName
 
-echo "Moving the backup file into the rescue-pod"
-kubectl cp --namespace=$namespace /tmp/backup.tar.gz rescue-pod:/tmp/
+echo "Moving the backup file into the $rescuePodName"
+kubectl cp --namespace=$namespace $localBackupFilePath $rescuePodName:/tmp/backup.tar.gz
 
-echo "Empty /tmp/jenkins-home of all files and folders on pvc $persistentVolumeClaim"
-kubectl exec --namespace=$namespace rescue-pod -- find /tmp/jenkins-home -type f -name "*.*" -delete || echo "Files deleted in jenkins-home"
-kubectl exec --namespace=$namespace rescue-pod -- find /tmp/jenkins-home -type f -name "*" -delete || echo "Files deleted in jenkins-home"
-kubectl exec --namespace=$namespace rescue-pod -- find /tmp/jenkins-home/ -mindepth 1 -type d -name "*" -exec rm -rf {} \; || echo "Folders deleted in jenkins-home"
+echo "Empty $rescueStorageMountPath of all files and folders on pvc $persistentVolumeClaim"
+kubectl exec --namespace=$namespace $rescuePodName -- find $rescueStorageMountPath -type f -name "*.*" -delete || echo "Files deleted in jenkins-home"
+kubectl exec --namespace=$namespace $rescuePodName -- find $rescueStorageMountPath -type f -name "*" -delete || echo "Files deleted in jenkins-home"
+kubectl exec --namespace=$namespace $rescuePodName -- find $rescueStorageMountPath -mindepth 1 -type d -name "*" -exec rm -rf {} \; || echo "Folders deleted in jenkins-home"
 
-echo "Uncompress the backup file into /tmp/jenkins-home"
-kubectl exec --namespace=$namespace rescue-pod -- tar -xzf /tmp/backup.tar.gz --directory /tmp/jenkins-home
+echo "Uncompress the backup file into $rescueStorageMountPath"
+kubectl exec --namespace=$namespace $rescuePodName -- tar -xzf /tmp/backup.tar.gz --directory $rescueStorageMountPath
 
 # OPTIONAL - enable if permissions are not set correctly, securityContext in the rescuepod should set permissions appropriately
 #echo "Update ownership permissions recursively"
-#kubectl exec --namespace=$namespace rescue-pod -- chown -R 1000:1000 /tmp/jenkins-home
+#kubectl exec --namespace=$namespace $rescuePodName -- chown -R 1000:1000 $rescueStorageMountPath
 
-echo "Deleting the rescue-pod"
-kubectl --namespace=$namespace delete pod rescue-pod
+echo "Deleting the $rescuePodName"
+kubectl --namespace=$namespace delete pod $rescuePodName
 
 echo "Scale up CJOC pods to 1 replica"
 kubectl --namespace=$namespace scale statefulset/$instanceStatefulsetName --replicas=1

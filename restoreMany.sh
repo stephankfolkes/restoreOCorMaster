2fc7409c61e47ce2ea52413dbd9d4e4a7386c4e1
Stephan Folkes <sfolkes@cloudbees.com>
2021-05-28 12:47:00 +0100

    Add quotes to file path to allow spaces

--- restoreMany.sh
+++ restoreMany.sh
@@ -17,9 +17,9 @@ do
   echo "Restoring $controllerStatefulset from $backupSource backup".
 
   if [[ $backupSource = "local" ]]; then
-    bash restore.sh --namespace $namespace --instanceStatefulsetName $controllerStatefulset --backupFilePath $filePath --backupSource $backupSource --rescueContainerImage $rescueContainerImage > "$cloudLocalDownloadDir/logs/$controllerStatefulset-restore.log" 2>&1 &
+    bash restore.sh --namespace $namespace --instanceStatefulsetName $controllerStatefulset --backupFilePath "$filePath" --backupSource $backupSource --rescueContainerImage $rescueContainerImage > "$cloudLocalDownloadDir/logs/$controllerStatefulset-restore.log" 2>&1 &
   elif [[ $backupSource = "s3" ]]; then
-    bash restore.sh --namespace $namespace --instanceStatefulsetName $controllerStatefulset --s3FilePath $filePath --s3BucketName $s3BucketName --backupSource $backupSource --cloudLocalDownloadDir $cloudLocalDownloadDir --rescueContainerImage $rescueContainerImage > "$cloudLocalDownloadDir/logs/$controllerStatefulset-restore.log" 2>&1 &
+    bash restore.sh --namespace $namespace --instanceStatefulsetName $controllerStatefulset --s3FilePath "$filePath" --s3BucketName $s3BucketName --backupSource $backupSource --cloudLocalDownloadDir $cloudLocalDownloadDir --rescueContainerImage $rescueContainerImage > "$cloudLocalDownloadDir/logs/$controllerStatefulset-restore.log" 2>&1 &
   fi
 done < $controllerList
 

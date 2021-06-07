adc7b7d55f36c98c9d310ab5af5cc51e0f65f6e1
Stephan Folkes <sfolkes@cloudbees.com>
2021-06-07 10:57:50 +0100

    Changing restoreMany from parallel to sequential

--- restoreMany.sh
+++ restoreMany.sh
@@ -18,9 +18,9 @@ do
   echo "Read log for $controllerStatefulset at: $cloudLocalDownloadDir/logs/$controllerStatefulset-restore.log"
 
   if [[ $backupSource = "local" ]]; then
-    bash restore.sh --namespace $namespace --instanceStatefulsetName $controllerStatefulset --backupFilePath "$filePath" --backupSource $backupSource --rescueContainerImage $rescueContainerImage > "$cloudLocalDownloadDir/logs/$controllerStatefulset-restore.log" 2>&1 &
+    bash restore.sh --namespace $namespace --instanceStatefulsetName $controllerStatefulset --backupFilePath "$filePath" --backupSource $backupSource --rescueContainerImage $rescueContainerImage > "$cloudLocalDownloadDir/logs/$controllerStatefulset-restore.log" 2>&1
   elif [[ $backupSource = "s3" ]]; then
-    bash restore.sh --namespace $namespace --instanceStatefulsetName $controllerStatefulset --s3FilePath "$filePath" --s3BucketName $s3BucketName --backupSource $backupSource --cloudLocalDownloadDir $cloudLocalDownloadDir --rescueContainerImage $rescueContainerImage > "$cloudLocalDownloadDir/logs/$controllerStatefulset-restore.log" 2>&1 &
+    bash restore.sh --namespace $namespace --instanceStatefulsetName $controllerStatefulset --s3FilePath "$filePath" --s3BucketName $s3BucketName --backupSource $backupSource --cloudLocalDownloadDir $cloudLocalDownloadDir --rescueContainerImage $rescueContainerImage > "$cloudLocalDownloadDir/logs/$controllerStatefulset-restore.log" 2>&1
   fi
 done < $controllerList
 
